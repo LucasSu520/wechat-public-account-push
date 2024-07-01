fd190a0e5e66d0c0322202e170a382ae8372a5a0
LucasSu520 <74417695+LucasSu520@users.noreply.github.com>
2024-07-01 19:47:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,3 +1,38 @@
-{"APP_ID":"wx683cc4deaab30164",
-  "APP_SECRET":"48faeed1e2865415167a8d6ffe3f74b0",
-  "IS_SHOW_COLOR":true,"CALLBACK_TEMPLATE_ID":"","CALLBACK_USERS":[{"name":"自己","id":""}],"USERS":[{"name":"全世界最可爱的小王","id":"oJ7et6GwmnNE1o_vxvAzzUBbX3gI","useTemplateId":"ozgQkzFJ6FAx_0YWxsutJMZh9bRm0citiKLq8RI48Sk","province":"四川省","city":"南充市","horoscopeDate":"12-23","horoscopeDateType":"本周","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"生日","name":"小王生日","date":"12-23","year":"2002"}],"customizedDateList":[]},{"name":"Lucas","id":"oJ7et6EmYNCfW62ts4ul8-7yWeSA","useTemplateId":"ozgQkzFJ6FAx_0YWxsutJMZh9bRm0citiKLq8RI48Sk","province":"上海市","city":"上海市","horoscopeDate":"07-26","horoscopeDateType":"本周","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"*生日","name":"Lucas生日","date":"06-14","year":"1999"}],"customizedDateList":[]}],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":true,"oneTalk":true,"earthyLoveWords":true,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":true,"birthdayMessage":true}}
+const USER_CONFIG ={
+  APP_ID: 'wx683cc4deaab30164',
+  APP_SECRET: '48faeed1e2865415167a8d6ffe3f74b0',
+  IS_SHOW_COLOR: true,
+  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_USERS: [{"name":"自己","id":""}],
+  USERS: [
+    {
+      name: '全世界最可爱的小王', 
+      id: 'oJ7et6GwmnNE1o_vxvAzzUBbX3gI',
+      useTemplateId: 'ozgQkzFJ6FAx_0YWxsutJMZh9bRm0citiKLq8RI48Sk',
+      province: '四川省',
+      city: '南充市',
+      horoscopeDate: "12-23",
+      horoscopeDateType: "本周",
+      openUrl:"https://shuangxunian.github.io/",
+      festivals:[
+        {
+          type:"生日", name:"小王生日", date: "12-23", year: "2002"
+        },
+        {
+        {
+          type: '节日', name: '在一起日子', year: '2024', date: '04-17',
+        }  
+        }
+      ],customizedDateList:[]},
+    {
+      name: "Lucas",
+      id: "oJ7et6EmYNCfW62ts4ul8-7yWeSA",
+      useTemplateId: "ozgQkzFJ6FAx_0YWxsutJMZh9bRm0citiKLq8RI48Sk",
+      province: "上海市",
+      city: "上海市",
+      horoscopeDate:"07-26",
+      horoscopeDateType:"本周",
+      "openUrl":"https://shuangxunian.github.io/",
+      "festivals":[{"type":"*生日","name":"Lucas生日","date":"06-14","year":"1999"}],"customizedDateList":[]}],
+  "SWITCH":{"weather":true,"holidaytts":true,"CIBA":true,"oneTalk":true,"earthyLoveWords":true,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":true,"birthdayMessage":true}}
+
